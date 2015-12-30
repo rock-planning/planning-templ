20dcf9e9e8ff7d6d4d42e1c5a19163c4e61517cc
Thomas Roehr <thomas.roehr@dfki.de>
2015-12-30 14:44:08 +0100

    Use define USE_BOOST_SHARED_PTR to switch between implementations

--- src/SharedPtr.hpp
+++ src/SharedPtr.hpp
@@ -1,6 +1,11 @@
-#pragma once
+#ifndef TEMPL_SHARED_PTR_HPP
+#define TEMPL_SHARED_PTR_HPP
 
 #if __cplusplus <= 199711L
+#define USE_BOOST_SHARED_PTR
+#endif
+
+#ifdef USE_BOOST_SHARED_PTR
 #include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
 #include <boost/make_shared.hpp>
@@ -11,7 +16,7 @@
 
 namespace templ
 {
-#if __cplusplus <= 199711L
+#ifdef USE_BOOST_SHARED_PTR
     using ::boost::shared_ptr;
     using ::boost::make_shared;
     using ::boost::dynamic_pointer_cast;
@@ -26,3 +31,5 @@ namespace templ
     using function1 = ::std::function<T(U)>;
 #endif
 }
+
+#endif // TEMPL_SHARED_PTR_HPP
