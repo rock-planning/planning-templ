35a64639c6cad90089041cad4a492fe5a350c857
Thomas Roehr <thomas.roehr@dfki.de>
2016-03-23 19:20:41 +0100

    Add SpaceTimeNetwork definition in own header

---
+++ src/SpaceTimeNetwork.hpp
@@ -0,0 +1,11 @@
+#ifndef TEMPL_SPACE_TIME_NETWORK_HPP
+#define TEMPL_SPACE_TIME_NETWORK_HPP
+
+#include <templ/TemporallyExpandedNetwork.hpp>
+#include <templ/symbols/constants/Location.hpp>
+
+namespace templ 
+{
+    typedef TemporallyExpandedNetwork<templ::symbols::constants::Location> SpaceTimeNetwork;
+} // end namespace templ
+#endif // TEMPL_SPACE_TIME_NETWORK_HPP
