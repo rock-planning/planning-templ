4aa6346d1e7de197d81ea14ec6ed0f6c2d54c243
Thomas Roehr <thomas.roehr@dfki.de>
2016-03-23 10:41:24 +0100

    Add test to validate value symmetry breaking

--- test/solvers/test_CSP.cpp
+++ test/solvers/test_CSP.cpp
@@ -248,4 +248,56 @@ BOOST_AUTO_TEST_CASE(mission_tt)
     }
 }
 
+BOOST_AUTO_TEST_CASE(symmetry_breaking)
+{
+    // mission outline -- qualitative planning
+    // [location_image_provider, mission_point1]@[t0,t1]
+    // [location_image_provider, mission_point2]@[t2,t3]
+    //
+    // temporalConstraint t1 < t2
+    //
+    // --> translate into systems and update timings accordingly
+    // --> mission should contain sychronization points
+
+    organization_model::OrganizationModel::Ptr om = organization_model::OrganizationModel::getInstance(
+                getRootDir() + "test/data/om-schema-latest.owl");
+    owlapi::model::IRI payloadModel = vocabulary::OM::resolve("Payload");
+
+    using namespace solvers::temporal;
+    point_algebra::TimePoint::Ptr t0 = point_algebra::QualitativeTimePoint::getInstance("t0");
+    point_algebra::TimePoint::Ptr t1 = point_algebra::QualitativeTimePoint::getInstance("t1");
+    point_algebra::TimePoint::Ptr t2 = point_algebra::QualitativeTimePoint::getInstance("t2");
+    point_algebra::TimePoint::Ptr t3 = point_algebra::QualitativeTimePoint::getInstance("t3");
+    point_algebra::TimePoint::Ptr t4 = point_algebra::QualitativeTimePoint::getInstance("t4");
+    point_algebra::TimePoint::Ptr t5 = point_algebra::QualitativeTimePoint::getInstance("t5");
+
+    using namespace ::templ::symbols;
+    constants::Location::Ptr loc0(new constants::Location("loc0", base::Point(0,0,0)));
+    constants::Location::Ptr loc1(new constants::Location("loc1", base::Point(10,10,0)));
+    constants::Location::Ptr loc2(new constants::Location("loc2", base::Point(10,15,0)));
+
+    Mission mission(om);
+    mission.addResourceLocationCardinalityConstraint(loc0, t0, t1, payloadModel);
+    mission.addResourceLocationCardinalityConstraint(loc1, t2, t3, payloadModel);
+    mission.addResourceLocationCardinalityConstraint(loc2, t4, t5, payloadModel);
+
+    // Overlapping intervals at two locations
+    mission.addTemporalConstraint(t2,t1, point_algebra::QualitativeTimePointConstraint::Greater);
+    mission.addTemporalConstraint(t4,t3, point_algebra::QualitativeTimePointConstraint::Less);
+
+    using namespace solvers;
+    {
+        organization_model::ModelPool modelPool;
+        modelPool[ vocabulary::OM::resolve("Payload") ] = 100;
+        mission.setAvailableResources(modelPool);
+
+        std::vector<solvers::csp::ModelDistribution::Solution> solutions = solvers::csp::ModelDistribution::solve(mission);
+        BOOST_REQUIRE_MESSAGE(solutions.size() == 1, "Exactly one solution found " << solutions);
+
+        // Check role distribution after model distribution
+        std::vector<solvers::csp::RoleDistribution::Solution> roleSolutions = solvers::csp::RoleDistribution::solve(mission, solutions[0]);
+        BOOST_REQUIRE_MESSAGE(roleSolutions.size(), "Exactly one solution found (using symmetry breaking): # of found solutions " << roleSolutions.size());
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
