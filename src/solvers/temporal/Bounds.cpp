737da28a21fdc47c0bdba5effc00d4761580cc35
Thomas Roehr <thomas.roehr@dfki.de>
2017-06-20 17:48:31 +0200

    Update Bounds

--- src/solvers/temporal/Bounds.cpp
+++ src/solvers/temporal/Bounds.cpp
@@ -1,4 +1,5 @@
 #include "Bounds.hpp"
+#include <sstream>
 
 namespace templ {
 namespace solvers {
@@ -8,6 +9,48 @@ Bounds::Bounds(double lowerBound, double upperBound)
     : mLowerBound(lowerBound)
     , mUpperBound(upperBound)
 {
+}
+
+std::string Bounds::toString(size_t indent) const
+{
+    std::string hspace(indent,' ');
+    std::stringstream ss;
+    ss << "[" << getLowerBound();
+    ss << "," << getUpperBound() << "]";
+    return ss.str();
+}
+
+std::string Bounds::toString(const List& list, size_t indent)
+{
+    std::string hspace(indent,' ');
+    std::stringstream ss;
+    ss << hspace << "Bounds:" << std::endl;
+    for(const Bounds& bounds : list)
+    {
+        ss << bounds.toString(indent + 4) << std::endl;;
+    }
+    return ss.str();
+}
+
+bool Bounds::operator<(const Bounds& other) const
+{
+    if(getLowerBound() < other.getLowerBound())
+    {
+        return true;
+    } else if(getLowerBound() == other.getLowerBound())
+    {
+        return getUpperBound() < other.getUpperBound();
+    } else {
+        return false;
+    }
+}
+
+bool Bounds::overlaps(const Bounds& other) const
+{
+    if(mLowerBound == other.mLowerBound || mUpperBound == other.mUpperBound)
+        return true;
+
+    return !(mUpperBound <= other.mLowerBound || other.mUpperBound <= mLowerBound);
 }
 
 } // end namespace temporal
--- src/solvers/temporal/Bounds.hpp
+++ src/solvers/temporal/Bounds.hpp
@@ -1,6 +1,9 @@
 #ifndef TEMPL_SOLVERS_TEMPORAL_BOUNDS_HPP
 #define TEMPL_SOLVERS_TEMPORAL_BOUNDS_HPP
 
+#include <vector>
+#include <string>
+
 namespace templ {
 namespace solvers {
 namespace temporal {
@@ -15,10 +18,29 @@ class Bounds
     double mUpperBound;
 
 public:
+    typedef std::vector<Bounds> List;
+
+    Bounds() {}
+
     Bounds(double lowerBound, double upperBound);
 
-    double getLowerBound() const { return mLowerBound; } 
+    void setLowerBound(double l) { mLowerBound = l; }
+    void setUpperBound(double l) { mUpperBound = l; }
+
+    double getLowerBound() const { return mLowerBound; }
     double getUpperBound() const { return mUpperBound; }
+
+    bool operator==(const Bounds& other) const { return mLowerBound == other.mLowerBound &&
+        mUpperBound == other.mUpperBound; }
+
+    bool operator<(const Bounds& other) const;
+
+    bool overlaps(const Bounds& other) const;
+
+    bool isValid() const { return mLowerBound <= mUpperBound; }
+
+    std::string toString(size_t indent = 0) const;
+    static std::string toString(const List& list, size_t indent = 0);
 };
 
 } // end namespace temporal
