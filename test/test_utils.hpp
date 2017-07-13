af0c9877de950aac781eff6714a913c2bf6f475d
Thomas Roehr <thomas.roehr@dfki.de>
2017-07-13 12:35:51 +0200

    test: fix documentation in test utils

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -3,8 +3,8 @@
 
 #include <string>
 /**
- * Retrieve the root directory by analysing the current execution path: 
- * returns /../organization_model/ as path
+ * Retrieve the root directory by analysing the current execution path:
+ * returns /../templ/ as path
  */
 std::string getRootDir();
 #endif
