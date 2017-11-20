0f7f24fec510f89052a5aa354704863fdcf2b993
Thomas Roehr <thomas.roehr@dfki.de>
2017-11-20 14:10:04 +0100

    benchmark: account for depots in mission generator

--- src/benchmark/MissionGenerator.cpp
+++ src/benchmark/MissionGenerator.cpp
@@ -1,8 +1,9 @@
 #include "MissionGenerator.hpp"
 #include <sstream>
 #include <organization_model/OrganizationModel.hpp>
-#include "../symbols/constants/Location.hpp"
 #include <organization_model/vocabularies/VRP.hpp>
+
+#include "../symbols/constants/Location.hpp"
 
 using namespace organization_model;
 using namespace templ::symbols;
@@ -29,7 +30,8 @@ Mission::Ptr MissionGenerator::convert(const VRPProblem& vrp)
     availableResources[vocabulary::VRP::Vehicle()] = vrp.getVehicles();
     mission->setAvailableResources(availableResources);
 
-
+    std::map<Coord2D, constants::Location::Ptr> coordLocationMap;
+    std::vector<TimePoint::Ptr> timepoints;
     for(const Coord2D& coord : vrp.getNodeCoordinates())
     {
         // Add locations -- nodes
@@ -37,7 +39,6 @@ Mission::Ptr MissionGenerator::convert(const VRPProblem& vrp)
         position.x() = coord.x;
         position.y() = coord.y;
         position.z() = 0;
-        bool metricDefinition = true;
 
         std::stringstream ss;
         ss << "l" << ++idx;
@@ -45,31 +46,92 @@ Mission::Ptr MissionGenerator::convert(const VRPProblem& vrp)
         constants::Location::Ptr location = constants::Location::create(ss.str(), position);
         mission->addConstant(location);
 
+        coordLocationMap[coord] = location;
+
         std::stringstream ssFrom;
         ssFrom << "t" << idx << "-0";
         std::stringstream ssTo;
         ssTo << "t" << idx << "-1";
         TimePoint::Ptr from = tcn->getOrCreateTimePoint(ssFrom.str());
+        timepoints.push_back(from);
         TimePoint::Ptr to = tcn->getOrCreateTimePoint(ssTo.str());
+        timepoints.push_back(to);
 
-        if(!endOfFirstInterval)
-        {
-            endOfFirstInterval = to;
-        } else {
-            try {
-                // making sure that depot is the first to be considered
-                solvers::Constraint::Ptr constraint0 = tcn->addQualitativeConstraint(endOfFirstInterval, from, QualitativeTimePointConstraint::Less);
-                mission->addConstraint(constraint0);
-            } catch(...)
-            {}
-        }
-
-
-        TemporalAssertion::Ptr temporalAssertion = mission->addResourceLocationCardinalityConstraint(location,
+        mission->addResourceLocationCardinalityConstraint(location,
                 from, to,
                 vocabulary::VRP::Commodity(),
                 vrp.getDemands()[idx-1],
                 owlapi::model::OWLCardinalityRestriction::MIN);
+    }
+
+    size_t depotIdx = 0;
+    TimePoint::Ptr startFrom;
+    TimePoint::Ptr startTo;
+    for(const Coord2D& coord : vrp.getDepots())
+    {
+        std::map<Coord2D, constants::Location::Ptr>::const_iterator cit = coordLocationMap.find(coord);
+        constants::Location::Ptr depotLocation;
+        if(cit != coordLocationMap.end())
+        {
+            depotLocation = cit->second;
+            startFrom = timepoints[0];
+            startTo = timepoints[1];
+        } else {
+
+            base::Point position;
+            position.x() = coord.x;
+            position.y() = coord.y;
+            position.z() = 0;
+
+            startFrom = tcn->getOrCreateTimePoint("t-init-0");
+            startTo = tcn->getOrCreateTimePoint("t-init-1");
+
+            std::stringstream ss;
+            ss << "depot" << depotIdx++;
+
+            depotLocation = constants::Location::create(ss.str(), position);
+            mission->addConstant(depotLocation);
+        }
+
+        // Making sure vehicles are available at starting locations
+        mission->addResourceLocationCardinalityConstraint(depotLocation,
+                startFrom,
+                startTo,
+                vocabulary::VRP::Vehicle(),
+                vrp.getVehicles(),
+                owlapi::model::OWLCardinalityRestriction::MAX);
+
+        mission->addResourceLocationCardinalityConstraint(depotLocation,
+                startFrom,
+                startTo,
+                vocabulary::VRP::Vehicle(),
+                vrp.getVehicles(),
+                owlapi::model::OWLCardinalityRestriction::MIN);
+
+        // Making sure commodities are available at starting locations
+        mission->addResourceLocationCardinalityConstraint(depotLocation,
+                startFrom,
+                startTo,
+                vocabulary::VRP::Commodity(),
+                vrp.getTotalDemand(),
+                owlapi::model::OWLCardinalityRestriction::MIN);
+
+        mission->addResourceLocationCardinalityConstraint(depotLocation,
+                startFrom,
+                startTo,
+                vocabulary::VRP::Commodity(),
+                vrp.getTotalDemand(),
+                owlapi::model::OWLCardinalityRestriction::MAX);
+    }
+
+    for(point_algebra::TimePoint::Ptr t : timepoints)
+    {
+        try {
+            // making sure that depot is the first to be considered
+            solvers::Constraint::Ptr constraint0 = tcn->addQualitativeConstraint(startTo, t, QualitativeTimePointConstraint::Less);
+            mission->addConstraint(constraint0);
+        } catch(...)
+        {}
     }
 
     return mission;
