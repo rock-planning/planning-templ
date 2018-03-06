f838b22bb893aa97c80b53c2089e9fe0268d03e3
Thomas Roehr <thomas.roehr@dfki.de>
2018-03-06 18:02:53 +0100

    csp: update documentation for utils::Converter

--- src/solvers/csp/utils/Converter.hpp
+++ src/solvers/csp/utils/Converter.hpp
@@ -8,10 +8,24 @@ namespace utils {
 class Converter
 {
 public:
+/**
+ * Create a set containing the cardinality vectors that corresponding the set of model
+ * pool combination
+ * \return set of vectors
+ */
 static std::set< std::vector<uint32_t> > toCSP(const Mission::Ptr& mission, const organization_model::ModelPool::Set& combinations);
 
+/**
+ * Create a fully expanded vector sized the number of the missions available resources
+ * The model pool updates the cardinality of relevant resources
+ * \return a vector of resource cardinalities
+ */
 static std::vector<uint32_t> toCSP(const Mission::Ptr& mission, const organization_model::ModelPool& combination);
 
+/**
+ * Get the model index in the available models of the mission
+ * \return index
+ */
 static uint32_t systemModelToCSP(const Mission::Ptr& mission, const owlapi::model::IRI& model);
 
 };
