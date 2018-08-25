5c351981bfe78baa43e655e52818716a1d988b10
Thomas Roehr <thomas.roehr@dfki.de>
2018-08-25 22:21:53 +0200

    utils: cleanup transport network planner

--- src/utils/TransportNetworkPlannerMain.cpp
+++ src/utils/TransportNetworkPlannerMain.cpp
@@ -89,72 +89,12 @@ int main(int argc, char** argv)
 
 
     std::vector<solvers::csp::TransportNetwork::Solution> solutions = solvers::csp::TransportNetwork::solve(mission,minimumNumberOfSolutions, configuration);
-    if(solutions.empty())
-    {
-        std::cout << "No solution found" << std::endl;
-    //} else {
-    //    std::cout << solutions.size() << " solutions have been found" << std::endl; // << solutions;
 
-    //    for(size_t i = 0; i < solutions.size(); ++i)
-    //    {
-    //        std::string filename;
-    //        try {
-    //            std::stringstream ss;
-    //            ss << "templ-mission-solution-" << i << ".dot";
-    //            filename = mission->getLogger()->filename(ss.str());
-    //            graph_analysis::io::GraphIO::write(filename, solutions[i].getMinCostFlowSolution().getGraph());
-    //        } catch(const std::exception& e)
-    //        {
-    //            std::cout << "Saving file " << filename << " failed: -- " << e.what();
-    //        }
-
-    //        try {
-    //            std::stringstream ss;
-    //            ss << "templ-mission-solution-" << i << ".gexf";
-    //            filename = mission->getLogger()->filename(ss.str());
-    //            solutions[i].getMinCostFlowSolution().save(filename, "gexf");
-    //        } catch(const std::exception& e)
-    //        {
-    //            std::cout << "Saving file " << filename << " failed: -- " << e.what();
-    //        }
-
-
-    //        std::cout << "Solution analysis" << std::endl;
-    //        solvers::SolutionAnalysis sa(mission, solutions[i].getMinCostFlowSolution());
-    //        std::cout << "    Required roles: " << Role::toString(sa.getRequiredRoles()) << std::endl;
-
-    //        for(const symbols::constants::Location::Ptr& location : mission->getLocations())
-    //        {
-    //            for(const solvers::temporal::Interval& interval : mission->getTimeIntervals())
-    //            {
-
-    //                std::cout << "At location: " << location->toString() << std::endl;
-    //                std::cout << "     " << interval.toString() << std::endl;
-
-    //                std::vector<organization_model::ModelPool> pools = sa.getAvailableResources(location, interval);
-
-    //                for(const organization_model::ModelPool& pool : pools)
-    //                {
-    //                    std::cout << pool.toString(8) << std::endl;
-    //                }
-    //            }
-    //        }
-
-    //        graph_analysis::BaseGraph::Ptr hyperGraph = sa.toHyperGraph();
-    //        try {
-    //            std::stringstream ss;
-    //            ss << "templ-mission-solution-hypergraph" << i << ".dot";
-    //            filename = mission->getLogger()->filename(ss.str());
-    //            graph_analysis::io::GraphIO::write(filename, hyperGraph);
-    //        } catch(const std::exception& e)
-    //        {
-    //            std::cout << "Saving file " << filename << " failed: -- " << e.what();
-    //        }
-
-    //    }
-
-    //    mission->getLogger()->disableSessions();
-    }
+    std::cout << "TemPl:" << std::endl;
+    std::cout << "    # of solutions found: " << solutions.size()
+        << std::endl
+        << "    Check log directory: " << mission->getLogger()->getBasePath()
+        << std::endl;
 
     return 0;
 }
